f7892dff5336169f02bee40281a1b70286c1e453
George Samuel <geosam2708@gmail.com>
2023-03-16 19:17:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+     customerName.toUpperCase();
+};
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+};
+
+const leastFavoriteCustomer = 'You Me';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Not You';
+};
+
+upperCaseCustomerName();
